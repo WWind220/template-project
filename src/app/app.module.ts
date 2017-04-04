6472b6ff35db9646c3ac77ac4dc0e5a2756da4c9
Oksana <obesarab@amcbridge.com>
2017-04-04 15:12:43 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,54 @@
+import {NgModule, ApplicationRef} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {AppComponent} from './app.component';
+import {ApiService} from './shared';
+import {ToastrModule} from 'ngx-toastr';
+import {removeNgStyles, createNewHosts} from '@angularclass/hmr';
+import {PinkToast} from './pink.toast';
+import {CustomToast} from './custom-toast/custom-toast.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
+  ],
+  declarations: [
+    AppComponent,
+    PinkToast,
+    CustomToast
+  ],
+  providers: [
+    ApiService
+  ],
+  entryComponents: [PinkToast, CustomToast],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor(public appRef: ApplicationRef) {
+  }
+
+  hmrOnInit(store) {
+    console.log('HMR store', store);
+  }
+
+  hmrOnDestroy(store) {
+    let cmpLocation = this.appRef.components.map(cmp => cmp.location.nativeElement);
+    // recreate elements
+    store.disposeOldHosts = createNewHosts(cmpLocation);
+    // remove styles
+    removeNgStyles();
+  }
+
+  hmrAfterDestroy(store) {
+    // display new elements
+    store.disposeOldHosts();
+    delete store.disposeOldHosts;
+  }
+}
